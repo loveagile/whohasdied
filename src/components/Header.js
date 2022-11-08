1f12beeae635f6de63096863ffe3f8c5250a6f5f
mark <happyplandry@gmail.com>
2022-11-07 23:28:06 -0500

    fixed: logo on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,11 @@ const Header = () => {
   const navigate = useNavigate()
 
   return (
-    <div className='box-content bg-[#f8f8f8] pl-[50px] pr-[50px]'>
+    <div className='box-content bg-[#f8f8f8]'>
       <div className='container flex justify-between h-28 w-full'>
-        <Link className='logo'>LOGO</Link>
+        <Link className='logo md:ml-[90px]'>
+          <img src='/logo.png' className='h-[25px] w-[151px]' alt='logo'></img>
+        </Link>
         <button className='article-btn'>Articles</button>
       </div>
     </div>
