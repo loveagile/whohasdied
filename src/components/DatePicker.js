ababe6f81157e9e5debdb93091ba44d27c96f8ee
mark <happyplandry@gmail.com>
2022-11-09 11:29:38 -0500

    added: datepicker

---
+++ src/components/DatePicker.js
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect } from 'react'
+import Datepicker from 'flowbite-datepicker/Datepicker'
+
+const DatePicker = (props) => {
+  const { name, label, type, value, setValue } = props
+  const datepickerRef = useRef()
+
+  useEffect(() => {
+    const datepicker = new Datepicker(datepickerRef.current, {})
+
+    const handleDateChange = (e) => {
+      setValue(name, e.target.value)
+    }
+    datepickerRef.current.addEventListener('changeDate', handleDateChange)
+
+    return () =>
+      datepickerRef.current.removeEventListener('changeDate', handleDateChange)
+  }, [])
+
+  const handleChange = (e) => {
+    e.stopPropagation()
+    setValue(name, e.target.value)
+  }
+
+  return (
+    <div className='relative z-0 mb-6 w-full group'>
+      <input
+        datepicker
+        name={name}
+        ref={datepickerRef}
+        type={type}
+        datepicker-format='dd/MM/yyyy'
+        value={value}
+        onChange={handleChange}
+        className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+      />
+      <label
+        htmlFor={name}
+        className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default DatePicker
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,10 @@ const SearchForm = (props) => {
   const handleSubmit = (value) => {}
   return (
     <form className='flex justify-center w-full' onSubmit={handleSubmit}>
-      <input className='search-input' placeholder='Search People'></input>
+      <input
+        className='search-input focus:outline-none'
+        placeholder='Search People'
+      ></input>
       <button
         type='submit'
         className='search-btn md:pl-[50px] md:pr-[50px] sm:pl-[50px] sm:pr-[50px]'
