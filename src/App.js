15fd7bafc56a1f1585a45d27ace5b741919c264d
mark <happyplandry@gmail.com>
2022-11-09 11:25:57 -0500

    added: flowbite module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import 'flowbite/dist/flowbite.js'
+import 'flowbite/dist/datepicker.js'
+import 'tw-elements/dist/js/index.min.js'
 
 import Layout from './components/Layout'
 import Router from './router'
