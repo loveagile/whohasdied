8259a84ddb9d7147c4ca98c90fb3f4994850dd66
mark <happyplandry@gmail.com>
2022-11-11 07:17:12 -0500

    fixed: config

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = `http://localhost:4000/api`
-export const SERVER_URL = `http://localhost:4000/`
+export const BASE_URL = `https://mighty-inlet-28758.herokuapp.com/api`
+export const SERVER_URL = `https://mighty-inlet-28758.herokuapp.com/`
