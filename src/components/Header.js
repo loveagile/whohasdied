22d48ffbb1e8c0ab74c258e2f9520f2b8fe973c6
mark <happyplandry@gmail.com>
2022-11-25 10:31:43 -0500

    fixed: logo and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='box-content bg-[#f8f8f8]'>
       <div className='flex justify-between h-28 w-full'>
         <Link className='logo ml-4 md:ml-[90px]' to='/'>
-          <img src='/logo.png' className='h-[25px] w-[151px]' alt='logo'></img>
+          <img src='/logo.png' className='h-[100px]' alt='logo'></img>
         </Link>
         <div className='flex'>
           <button className='article-btn mr-4 md:mr-[100px]'>Articles</button>
